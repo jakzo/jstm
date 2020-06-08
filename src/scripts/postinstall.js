3f2b2971134b6485e732e4f51106cf2bb65cdf56
jakzo <jack@jf.id.au>
2020-06-09 02:21:58 +1000

    fix build

--- src/scripts/postinstall.js
+++ src/scripts/postinstall.js
@@ -8,9 +8,9 @@ const distScriptExists = () => {
 };
 
 const main = async () => {
-  if (distScriptExists()) await require('../../dist/bin/project').main();
+  // Path mangling is so that Typescript does not try to these `require()`d files
+  if (distScriptExists()) await require('../../dist/bin/project' + '').main();
   // When developing in the @jakzo/project repo there is no `dist` on first run of `yarn`
-  // Path mangling is so that Typescript does not try to compile `setup.js`
   else await require('../../scripts/setup' + '').main();
 };
 
