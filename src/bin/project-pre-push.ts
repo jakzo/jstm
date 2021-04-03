9e9fecfcdb01d9059cb5d3b62d9da946b414cea4
jakzo <jack@jf.id.au>
2021-04-03 22:44:22 +1100

    fix release pipeline

--- src/bin/project-pre-push.ts
+++ src/bin/project-pre-push.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 import { readChangesetState } from "@changesets/release-utils";
 import chalk from "chalk";
+import inquirer from "inquirer";
 
 const forkPromise = (modulePath: string, args: string[]): Promise<void> =>
   new Promise((resolve, reject) => {
@@ -19,12 +20,28 @@ export const main = async (): Promise<void> => {
   if (changesets.length > 0) return;
 
   console.log(
-    chalk.blueBright("Enter changeset information or press escape to skip...")
+    chalk.yellow(
+      "Warning: changesets are required to merge to master but none were found."
+    )
   );
+  const { shouldCreate } = await inquirer.prompt<{ shouldCreate: boolean }>([
+    {
+      name: "shouldCreate",
+      message: "Would you like to generate one now?",
+      type: "confirm",
+      default: true,
+    },
+  ]);
+  if (!shouldCreate) return;
+
   await forkPromise(
     path.join(__dirname, "..", "..", "node_modules", ".bin", "changeset"),
     []
   );
+  console.log(
+    chalk.blueBright("Please run `git push` again to push with changelog.")
+  );
+  process.exit(1);
 };
 
 main().catch((err) => {
