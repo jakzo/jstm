71210dd271e30006a6e63a54a83ce5dddf7c6ed4
jakzo <jack@jf.id.au>
2021-04-08 22:40:39 +1000

    fix changelog entries in GitHub releases

---
+++ src/bin/ci-github-print-changelog.ts
@@ -0,0 +1,21 @@
+import { getChangelogEntry } from "@changesets/release-utils";
+import * as fse from "fs-extra";
+import { PackageJson } from "type-fest";
+
+export const main = async (): Promise<void> => {
+  const changelog = await fse.readFile("CHANGELOG.md", "utf8");
+  const { version } = (await fse.readJson("package.json")) as PackageJson;
+  if (!version) throw new Error("version missing from package.json");
+  const { content } = getChangelogEntry(changelog, version);
+  console.log(
+    content.replace(
+      /[%\r\n]/g,
+      (ch) => `'%${ch.charCodeAt(0).toString(16).padStart(2, "0")}'`
+    )
+  );
+};
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
