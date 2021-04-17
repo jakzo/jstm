7a76ded91357a9a439b401a74f5ac0b1009fd5b4
jakzo <jack@jf.id.au>
2021-04-17 14:30:15 +1000

    allow non-null assertions

--- src/generators/eslint.ts
+++ src/generators/eslint.ts
@@ -96,6 +96,7 @@ module.exports = {
     "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/restrict-template-expressions": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
 
     // Modified rules
     "no-constant-condition": ["error", { checkLoops: false }],
