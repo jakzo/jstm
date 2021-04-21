bbe9ad69c30882d7db3e82c158785873652f77d3
jakzo <jack@jf.id.au>
2021-04-21 21:34:38 +1000

    fix postinstall script

--- presets/template/project.ts
+++ presets/template/project.ts
@@ -4,6 +4,8 @@ import { applyPresetCli } from "@jstm/core";
 import packageJson from "./package.json";
 import preset from "./preset";
 
+if (process.env.INIT_CWD) process.chdir(process.env.INIT_CWD);
+
 applyPresetCli(preset, packageJson).catch((err) => {
   console.error(err);
   process.exit(1);
