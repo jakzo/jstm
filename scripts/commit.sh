92c0e689be8bd2c7a89993bc6496faa86ecb97b4
jakzo <jack@jf.id.au>
2021-06-05 13:14:54 +1000

    fix publish

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -eux
 
+echo "Commit and changelog: $1"
+yarn test:all
 git add -A
 git commit -m "$1"
 git pull
+RAND=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1)
 echo "---
 \"@jstm/core\": patch
 ---
 
 $1
-" > generated-changeset.md
+" > ".changeset/generated-changeset-$RAND.md"
+git add -A
+git commit -n -m "added changelog: $1"
 git push
